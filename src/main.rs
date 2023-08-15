428a1c841ee4d7a884bb294592a6f63ef8216cc5
jeffock <jeffock1656@gmail.com>
2023-08-15 16:59:08 -0400

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,5 @@
-use nu_protocol::ast::Call;
-use nu_protocol::engine::{CommandRegistry,EvaluationContext,ShellError};
-use nu_protocol::pipeline::Pipeline;
-use nu_protocol::Value;
-use nu_source::Tag;
+use nu_plugin_nufetch::NuFetch;
 
-use std::sync::Arc;
-
-//clear, ascii + sys info, compact (c) or normal (.)
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //empty command registry
-    let mut registry = CommandRegistry::new();
-
-    //evalution context
-    let context = EvaluationContext::basic()?;
-
-    //parese commands
-    let mut pipeline = Pipeline::new();
-    let call = Call {
-        head: nu_protocol::hir::Expression::external_command("clear"),
-        positional: vec![],
-        named: vec![],
-        span: Tag::unknown(),
-    };
-
-    //execute pipeline
-    let result = context.run_pipeline(
-        &mut registry,
-        Arc::new(pipeline),
-        Value::nothing(),
-    );
-
-    //handle result
-    match result {
-        Ok(Value::Stream(stream)) => {
-            for item in stream {
-                println!("{}",item.format(None));
-            }
-        }
-        Err(ShellError::UntaggedRuntimeError(msg)) => {
-            eprintln!("Error: {}",msg);
-        }
-        _ => {
-            eprintln!("An error occurred");
-        }
-    }
-
-    Ok(())
+fn main() {
+   serve_plugin(&mut NuFetch {})
 }
---
+++ src/nufetch.rs
@@ -0,0 +1,7 @@
+pub struct NuFetch;
+
+impl NuFetch {
+    pub fn nufetch_out() -> () {
+        println!("test");
+    }
+}
